56eaaa3ab19527d00d33ef6251fe93019edf14c8
HaeChan Kim <bluesun147@naver.com>
2022-03-12 21:54:16 +0900

    create mongoose model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -11,14 +11,35 @@ const User = mongoose.model('User', {
     }
 })
 
-const me = new User({ // 생성자
-    name: 'Haechan',
-    age: 24
+// const me = new User({ // 생성자
+//     name: 'Haechan',
+//     age: 24
+// })
+
+// me.save() // db에 데이터 저장. promise 리턴
+// .then(() => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log('Error!', error);
+// })
+
+const Task = mongoose.model('Task', { // define Task model
+    description: {
+        type: String // 자료형
+    },
+    completed: {
+        type: Boolean
+    }
 })
 
-me.save() // db에 데이터 저장. promise 리턴
+const task = new Task({ // create new instance of a model
+    description: 'Learn the Mongoose library!!',
+    completed: false
+})
+
+task.save()
 .then(() => {
-    console.log(me);
+    console.log(task);
 }).catch((error) => {
     console.log('Error!', error);
 })
