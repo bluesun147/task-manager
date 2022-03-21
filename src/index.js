0877544e0a2d3799472f3e87215647f44b335ced
HaeChan Kim <bluesun147@naver.com>
2022-03-21 23:09:33 +0900

    update (patch)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user'); // 유저 모델
 const Task = require('./models/task');
+const { findByIdAndUpdate } = require('./models/user');
 
 // 데이터베이스 연결: /Users/blues/mongodb/bin/./mongod.exe --dbpath=/Users/blues/mongodb-data
 
@@ -74,6 +75,33 @@ app.get('/users/:id', async (req, res) => {
     // })
 })
 
+app.patch('/users/:id', async (req, res) => { // http method, update
+    
+    const updates = Object.keys(req.body); // 키들 배열로 리턴
+
+    // 업데이트 허용할 값들
+    const allowedUpdates = ['name', 'email', 'password', 'age']; // 나머지는 invalid
+
+    const isValidOperation = updates.every((update) => { // 5개 모두 true 리턴하면 true, 하나라도 false면 false 리턴
+        return allowedUpdates.includes(update);
+    });
+
+    if (!isValidOperation) { // 모두 만족하지 못하면 (하나라도 false면)
+        return res.status(400).send({error: 'Invalid updates!'});
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true}) // id, 바꿀값, 옵션
+
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     console.log(req.body);
     const task = new Task(req.body);
@@ -129,6 +157,31 @@ app.get('/tasks/:id', async (req, res) => {
     // }).catch((e) => {
     //     res.status(500).send();
     // })
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body);
+
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates!'});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 })
 
 app.listen(port, () => {
